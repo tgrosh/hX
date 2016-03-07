c413f5582962d5667d21713aa272e3233aaf9d4d
tgrosh <tgrosh@yahoo.com>
2016-03-07 17:19:59 -0500

    Balance Updates

--- Assets/Scripts/PurchaseManager.cs
+++ Assets/Scripts/PurchaseManager.cs
@@ -17,8 +17,8 @@ public class PurchaseManager
     });
 
     public static Purchase UpgradeBooster = new Purchase(new List<PurchaseCost>() { 
-        new PurchaseCost(ResourceType.Corium, 2), 
-        new PurchaseCost(ResourceType.Hydrazine, 1) 
+        new PurchaseCost(ResourceType.Corium, 1), 
+        new PurchaseCost(ResourceType.Hydrazine, 2) 
     });
 
     public static Purchase UpgradeBlaster = new Purchase(new List<PurchaseCost>() { 
@@ -27,8 +27,8 @@ public class PurchaseManager
     });
 
     public static Purchase UpgradeTractorBeam = new Purchase(new List<PurchaseCost>() { 
-        new PurchaseCost(ResourceType.Supplies, 2), 
-        new PurchaseCost(ResourceType.Hydrazine, 1) 
+        new PurchaseCost(ResourceType.Workers, 1), 
+        new PurchaseCost(ResourceType.Hydrazine, 2) 
     });
 
 }
